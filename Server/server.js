c6b61e89246426bb8f2ec4d8fe5eb4ece813eed4
Sumit Chaudhari <sc216766@gmail.com>
2026-09-03 18:16:17 +0530

    feat: initialize Express server with database, Cloudinary, and route configurations

--- Server/server.js
+++ Server/server.js
@@ -21,9 +21,7 @@ const app = express();
 
 app.use(cors({
     origin: true,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    credentials: true
 }));
 
 app.post('/api/stripe' , express.raw({type: "application/json"}),stripeWebhooks)
